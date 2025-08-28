0ceeedcfd984eded54a2ae6549a513e4b2f38de1
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2025-08-28 15:28:41 -0300

    feat: adds initial App component structure with states for tasks

--- App.js
+++ App.js
@@ -2,3 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, FlatList, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+export default function App() {
+
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  
+}
