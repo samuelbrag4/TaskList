79c1226c266cd224919d1ff3892530eacfd913b0
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2025-08-28 15:30:35 -0300

    feat: implement task loading and saving functionality with AsyncStorage

--- App.js
+++ App.js
@@ -7,5 +7,31 @@ export default function App() {
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
 
-  
+  // Função para mostrar as tarefas salvas ao abrir o app
+  useEffect(() => {
+    async function loadTasks() {
+      const savedTasks = await AsyncStorage.getItem('tasks');
+      if (savedTasks) {
+        setTasks(JSON.parse(savedTasks));
+      }
+    }
+    loadTasks();
+  }, []);
+
+  // Variável para salvar as tarefas no AsyncStorage
+  const saveTasks = async (newTasks) => {
+    await AsyncStorage.setItem('tasks', JSON.stringify(newTasks));
+  };
+
+  // Variável que adicona uma nova tarefa
+  const addTask = () => {
+      if (task.trim() === '') {
+        Alert.alert('Erro', 'Digite uma tarefa antes de adicionar!');
+        return;
+      }
+      const newTasks = [...tasks, task];
+      setTasks(newTasks);
+      saveTasks(newTasks);
+      setTask('');
+    };
 }
